67555756d31fcae67f79fc17dbc9cc00839b0f07
so_minho <59383800+pot1223@users.noreply.github.com>
2022-11-12 15:18:19 +0900

    Update example2.py

--- LP(Linear Programming)/example2.py
+++ LP(Linear Programming)/example2.py
@@ -20,6 +20,8 @@ x = [solver.NumVar(0.0, solver.infinity(), food[0]) for food in food_list]
 print('Number of variables = ',solver.NumVariables())
 
 # 제약조건 정의
+# CT1 : 50 <= 10*x1 + 0*x2 + 20*x3 + 20*x4 + 10*x5 + 20*x6 <= infinity
+# CT2 : 60 <= 0*x1 + 10*x2 + 30*x3 + 10*x4+ 30*x5 + 20*x6 <= infinity
 for i in range(0, len(vitamin_list)):
   ct = 0 
   for j in range(0, len(food_list)):
